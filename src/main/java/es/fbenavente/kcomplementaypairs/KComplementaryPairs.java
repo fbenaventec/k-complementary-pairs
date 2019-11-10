ebecec3e04498288e1513391889df315873ca707
Felipe Benavente <fbenavente@activitiesbank.com>
2019-11-10 14:30:17 +0100

    Add first version of interface

--- src/main/java/es/fbenavente/kcomplementaypairs/KComplementaryPairs.java
+++ src/main/java/es/fbenavente/kcomplementaypairs/KComplementaryPairs.java
@@ -1,4 +1,10 @@
 package es.fbenavente.kcomplementaypairs;
 
+import java.util.Collections;
+import java.util.List;
+
 public class KComplementaryPairs {
+    public List<Pair<Integer>> getKComplementaryPairs(List<Integer> values, int k) {
+        return Collections.singletonList(new Pair<Integer>(0,2));
+    }
 }
--- src/main/java/es/fbenavente/kcomplementaypairs/Pair.java
+++ src/main/java/es/fbenavente/kcomplementaypairs/Pair.java
@@ -3,7 +3,7 @@ package es.fbenavente.kcomplementaypairs;
 import lombok.Value;
 
 @Value
-public class ComplementaryPair {
-    private final int i;
-    private final int j;
+public class Pair<T> {
+    private final T i;
+    private final T j;
 }
