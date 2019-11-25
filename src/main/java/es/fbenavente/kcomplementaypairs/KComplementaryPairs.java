649e22fd3437276505a6cca90ab7931277d91a9c
Felipe Benavente <fbenavente@activitiesbank.com>
2019-11-25 19:22:11 +0100

    Code review

--- src/main/java/es/fbenavente/kcomplementaypairs/KComplementaryPairs.java
+++ src/main/java/es/fbenavente/kcomplementaypairs/KComplementaryPairs.java
@@ -12,11 +12,15 @@ public class KComplementaryPairs {
             int iValue = values.get(i);
             for (int j = i + 1; j < values.size(); j++) {
                 int jValue = values.get(j);
-                if (k == iValue + jValue) {
+                if (isPairKComplementary(k, iValue, jValue)) {
                     pairs.add(new Pair<>(i, j));
                 }
             }
         }
         return pairs;
+    }
+
+    private boolean isPairKComplementary(int k, int iValue, int jValue) {
+        return k == iValue + jValue;
     }
 }
